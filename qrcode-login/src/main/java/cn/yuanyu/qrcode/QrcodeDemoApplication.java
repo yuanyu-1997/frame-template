6de8ac3817ba4175ee046dd12d248de1b7886252
yuanyu-1997 <1750762853@qq.com>
2020-08-21 20:25:50 +0800

    修改二维码登陆案例

---
+++ qrcode-login/src/main/java/cn/yuanyu/qrcode/QrcodeDemoApplication.java
@@ -0,0 +1,11 @@
+package cn.yuanyu.qrcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodeDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QrcodeDemoApplication.class, args);
+    }
+}
---
+++ qrcode-login/src/test/java/cn/yuanyu/qrcode/QrcodeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.yuanyu.qrcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class QrcodeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
